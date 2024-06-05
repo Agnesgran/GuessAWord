9822688419d29ee51fb8b59887e7684eceb1671e
Agnes Gran <agnesgrahn95@gmail.com>
2024-06-05 12:48:05 +0000

    add to get_word for selecting category

--- run.py
+++ run.py
@@ -10,24 +10,39 @@ def get_word():
         "colors": ["red", "blue", "green", "yellow", "purple"]
      }
     
+    while True:
+        # Display available categories
+        print("Choose a category: ", ', '.join(categories.keys()))
+        category = input().lower()
+
+        if category in categories:
+            # Return a random word from the chosen category
+            return random.choice(categories[category])
+        else:
+            print("Uh-oh! That category's not on the list. Try one of the given choices.")
+    
+def guess_a_word():
+    """
+    Main function to run the game. 
+    Manages game state, handles user input, and controls game flow.
+    """
+    
 def progress_update(word, guessed_letters):
     """
     Displays the current state of the game, 
     showing guessed letters and blanks for unguessed letters
     """ 
 
-def play_again():
-    """
-    Asks the player if they want to play again.
-    """
-
 def display_score(wins, losses):
     """
     Displays the current score of the player.
     """
 
-def guess_a_word():
+
+def play_again():
     """
-    Main function to run the game. 
-    Manages game state, handles user input, and controls game flow.
+    Asks the player if they want to play again.
     """
+
+
+
