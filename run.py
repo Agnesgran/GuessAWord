2a5a6b97f692146e1b96999e5f49f725d9d03f5f
Agnes Gran <agnesgrahn95@gmail.com>
2024-06-05 12:43:05 +0000

    add basic function structure and categories

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def get_word():
+    """
+    Selects a random word from a chosen category.
+    """
+    categories = {
+        "fruits": ["apple", "banana", "cherry", "date", "fig"],
+        "animals": ["dog", "cat", "elephant", "giraffe", "hippopotamus"],
+        "colors": ["red", "blue", "green", "yellow", "purple"]
+     }
+    
+def progress_update(word, guessed_letters):
+    """
+    Displays the current state of the game, 
+    showing guessed letters and blanks for unguessed letters
+    """ 
+
+def play_again():
+    """
+    Asks the player if they want to play again.
+    """
+
+def display_score(wins, losses):
+    """
+    Displays the current score of the player.
+    """
+
+def guess_a_word():
+    """
+    Main function to run the game. 
+    Manages game state, handles user input, and controls game flow.
+    """
